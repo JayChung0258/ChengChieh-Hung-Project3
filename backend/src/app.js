fc1753902f1b8893746c6279136d78438388cfd0
Jay Hung <37239758+JayChung0258@users.noreply.github.com>
2024-12-12 12:28:07 +0800

    fix: update settings

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
+// backend/app.js
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 const authMiddleware = require('./middleware/authMiddleware');
 
 const authRoutes = require('./routes/authRoutes');
@@ -10,14 +13,23 @@ const authCheckRoutes = require('./routes/authCheckRoutes');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    credentials: true 
+}));
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(authMiddleware);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/auth', authCheckRoutes);
-app.use('/api/posts', postRoutes);
-app.use('/api/users', userRoutes);
+app.use('/api/posts', authMiddleware, postRoutes);
+app.use('/api/users', authMiddleware, userRoutes);
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 module.exports = app;
